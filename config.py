c6e2b16646f932b151a578e71bc50a43e8ea2da1
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-06-28 10:34:46 +0200

    feat: added config file that will store all selectors needed for scraping

---
+++ config.py
@@ -0,0 +1,5 @@
+'''
+Config file containing all xpath selectors needed for scraping
+'''
+#Selector used to get product_name and url from category
+cat_product_selector = '//*[@class="_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 m6ax_n4 _6a66d_LX75-  m7er_k4 msa3_z4"]'
