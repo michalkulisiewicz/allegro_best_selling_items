e6bf37b7fdf4ab55264c48c3a626c02a8f70cde3
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-06-28 14:22:30 +0200

    feature: Added number of sold items selector

--- allegro_auction_scraper.py
+++ allegro_auction_scraper.py
@@ -9,5 +9,30 @@ class AllegroAuctionScraper:
     def __init__(self):
         self.driver = init_selenium()
 
-    def run_auction_scraper(self):
-        pass
+    def run_auction_scraper(self, url=None):
+        self.driver.get(url)
+        price_selector =
+        test = self.driver.find_element(By.XPATH, price_selector)
+        print('test')
+
+        # containers = [
+        #     "section#top-card div.content",  # product_image
+        #     "div.job-salary-container",  # product_price
+        #     "ul.company-growth-stats.stats-list",  # product_shipping_cost
+        #     "div.insights-card.applicants-skills"  # number_of_sold_items
+        # ]
+        # for container in containers:
+        #     try:
+        #         WebDriverWait(self.driver, .25).until(
+        #             EC.presence_of_element_located(
+        #                 (By.XPATH, container)
+        #             )
+        #         )
+        #     except Exception as e:
+        #         print("timeout error waiting for container to load or element" \
+        #               " not found: {}".format(container))
+        #         print(e)
+
+
+allegro_auction_scraper = AllegroAuctionScraper()
+allegro_auction_scraper.run_auction_scraper('https://allegro.pl/oferta/biustonosz-samonosny-push-up-bez-ramiaczek-oslonki-10852424622')
