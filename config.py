df2e741a9854825fcf4890a818dab66b5f2db0e6
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-06-28 11:22:57 +0200

    feture: added selector to get maximum number of pages in category

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ Config file containing all xpath selectors needed for scraping
 '''
 #Selector used to get product_name and url from category
 cat_product_selector = '//*[@class="_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 m6ax_n4 _6a66d_LX75-  m7er_k4 msa3_z4"]'
+#Selector used to get maximum number of pages in category
+cat_max_page_selector = '//*[@class="_1h7wt mh36_8 mvrt_8 _6d89c_3i0GV _6d89c_XEsAE"]'
