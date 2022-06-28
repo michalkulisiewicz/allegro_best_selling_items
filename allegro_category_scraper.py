3999a98e73c4dd4b1051be23f22e82fb855f7acc
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-06-28 13:50:03 +0200

    fix: Moved selenium initialization to other file

--- allegro_category_scraper.py
+++ allegro_category_scraper.py
@@ -1,11 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver import init_selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 import config
 import json
 from fake_useragent import UserAgent
@@ -14,7 +10,7 @@ from time import sleep
 
 class AllegroCategoryScraper:
     def __init__(self):
-        self.driver = self._init_selenium()
+        self.driver = init_selenium()
 
     def _save_dict_to_json_file(self, filename, dict):
         with open(filename, 'w') as f:
@@ -23,29 +19,6 @@ class AllegroCategoryScraper:
     def wait_and_click(self, path):
         WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, path))).click()
-
-    #initialize webdriver set settings that allows to hide browser automation
-    def _init_selenium(self):
-        try:
-            #Temporary version 102.0.5005.61 of chrome is used as the latest version does not work properly
-            service = ChromeService(executable_path=ChromeDriverManager(version='102.0.5005.61').install())
-            # service = ChromeService(executable_path='/home/donnie/PycharmProjects/allegro_best_selling_items/chromedriver')
-            options = Options()
-            options.add_argument('user-data-dir=session')
-            # For ChromeDriver version 79.0.3945.16 or over
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
-            options.add_experimental_option('useAutomationExtension', False)
-            options.add_argument("--disable-blink-features")
-            options.add_argument("--disable-blink-features=AutomationControlled")
-            options.add_argument("--start-maximized")
-            # options.add_argument('--headless')
-            driver = webdriver.Chrome(service=service, options=options)
-            # disable automation flag
-            # self.driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-            return driver
-        except Exception as e:
-            print('Error while initializing webdriver')
-            print(e)
 
     def _scrape_cat_page(self):
         """
@@ -132,6 +105,4 @@ class AllegroCategoryScraper:
 
 
 allegro_scraper = AllegroCategoryScraper()
-products = allegro_scraper.run_cat_scraper('https://allegro.pl/kategoria/bielizna-damska-ponczochy-76003', 2)
-allegro_scraper.save_dict_to_json_file('cat_scraper_output.json', products)
-
+products = allegro_scraper.run_cat_scraper('https://allegro.pl/kategoria/bielizna-damska-ponczochy-76003', 1)
