502ffefcfee6bb043b84672e45152eacf6d71f0c
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-07-01 15:22:06 +0200

    fix: Deleted unused imports

--- allegro_category_scraper.py
+++ allegro_category_scraper.py
@@ -4,8 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import config
 import json
-from fake_useragent import UserAgent
-from time import sleep
 
 
 class AllegroCategoryScraper:
