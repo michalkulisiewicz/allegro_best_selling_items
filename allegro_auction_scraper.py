feac67268e5d2d2d40206ccf29e749aae0736c4d
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-07-07 14:30:19 +0200

    Feature: Added _get_auction_number method

--- allegro_auction_scraper.py
+++ allegro_auction_scraper.py
@@ -6,10 +6,43 @@ from selenium.webdriver.support import expected_conditions as ec
 import config
 import json
 import re
+from pathlib import Path
 
 class AllegroAuctionScraper:
     def __init__(self):
         self.driver = init_selenium()
+
+    def _extract_digits_from_string(self, string):
+        return [int(s) for s in string.split() if s.isdigit()][0]
+
+    def _extract_cat_num_from_filename(self, filename):
+        category_num = filename.split('.')[0]
+        return int(category_num)
+
+    def _read_auctions_from_json(self):
+        file_list = list(Path('category_scraper_output').glob("*.json"))
+        auctions = {}
+        for file in file_list:
+            cat_num = self._extract_cat_num_from_filename(file.name)
+            with open(file, 'r') as f:
+                json_file = json.load(f)
+                auctions[cat_num] = json_file
+        return auctions
+
+
+    def _scroll_down_page(self):
+        try:
+            WebDriverWait(self.driver, 1).until(
+                ec.presence_of_element_located(
+                    (By.TAG_NAME, 'html')
+                )
+            )
+        except Exception as e:
+            print('timeout error waiting to located element by TAG_NAME: "html"')
+            print(e)
+        page = self.driver.find_element(By.TAG_NAME, 'html')
+        page.send_keys(Keys.PAGE_DOWN)
+        page.send_keys(Keys.PAGE_DOWN)
 
     def _get_product_price(self):
         '''
@@ -27,11 +60,11 @@ class AllegroAuctionScraper:
             print('Couldn\'t extract product price')
 
     def _get_shipping_price(self):
-        '''
+        """
         Selector returns price as: 'Dostawa od 6,70 zł'. Method extracts price swaps ',' with '.'
         return the price as a float in order to use it for further calculation
         :return: shipping price (float)
-        '''
+        """
         shipping_price = self.driver.find_element(By.XPATH, config.shipping_price_selector).text
         match = re.search('.\d+[,].\d', shipping_price)
         if match != None:
@@ -57,21 +90,6 @@ class AllegroAuctionScraper:
             print('Couldn\'t extract shipping price')
 
 
-    def _scroll_down_page(self):
-        try:
-                WebDriverWait(self.driver, .25).until(
-                    ec.presence_of_element_located(
-                        (By.TAG_NAME, 'html')
-                    )
-                )
-        except Exception as e:
-            print('timeout error waiting to located element by TAG_NAME: "html"')
-            print(e)
-        page = self.driver.find_element(By.TAG_NAME, 'html')
-        page.send_keys(Keys.PAGE_DOWN)
-        page.send_keys(Keys.PAGE_DOWN)
-
-
     def _get_name_of_the_seller(self):
         """
         Selector returns seller name as: 'od name_of_the_seller'. Method splits string to extract
@@ -92,36 +110,40 @@ class AllegroAuctionScraper:
         image = wait.until(ec.visibility_of_element_located((By.XPATH, config.product_img_selector))).get_attribute('src')
         return image
 
+    def _get_auction_number(self):
+        """
+        Selector returns auction number as: 'Numer oferty: 10182306603'. Method get digits from the string
+        and returns as an integer
+        :return: auction number (int)
+        """
+        auction_number_output = self.driver.find_element(By.XPATH, config.auction_number_selector).text
+        auction_number = self._extract_digits_from_string(auction_number_output)
+        return auction_number
 
     def run_auction_scraper(self, url=None):
-        self.driver.get(url)
-        self._scroll_down_page()
-        print(self._get_product_img_url())
-        # self._get_name_of_the_seller()
-        # product_price = self._get_product_price()
-        # print(product_price)
-        # shipping_price = self._get_shipping_price()
-        # print(shipping_price)
-
+        auctions_from_json = self._read_auctions_from_json()
+        scraped_auctions = []
+        for cat_num, auction_dict in auctions_from_json.items():
+            for auction_name, auction_url in auction_dict.items():
+                auction = {}
+                self.driver.get(auction_url)
+                print('Scraping auction from url: {}'.format(auction_url))
+                self._scroll_down_page()
+                auction['product_price'] = self._get_product_price()
+                auction['shipping_price'] = self._get_shipping_price()
+                auction['name_of_the_seller'] = self._get_name_of_the_seller()
+                auction['number_of_sold_items'] = self._get_number_of_sold_items()
+                auction['product_img_url'] = self._get_product_img_url()
+                auction['auction_number'] = self._get_auction_number()
+                scraped_auctions.append(auction)
         self.driver.close()
 
-        # containers = [
-        #     "section#top-card div.content",  # product_image
-        #     "div.job-salary-container",  # product_price
-        #     "ul.company-growth-stats.stats-list",  # product_shipping_cost
-        #     "div.insights-card.applicants-skills"  # number_of_sold_items
-        # ]
-        # for container in containers:
-        #     try:
-        #         WebDriverWait(self.driver, .25).until(
-        #             EC.presence_of_element_located(
-        #                 (By.XPATH, container)
-        #             )
-        #         )
-        #     except Exception as e:
-        #         print("timeout error waiting for container to load or element" \
-        #               " not found: {}".format(container))
-        #         print(e)
+        # self.driver.get(url)
+        # self._scroll_down_page()
+        #
+        # self.driver.close()
+
+
 
 
 allegro_auction_scraper = AllegroAuctionScraper()
