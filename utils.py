8fe6b216be9455618320243d006fbfb8eeafe1ff
michal_kulisiewicz <michalkulisiewicz@gmail.com>
2022-07-23 06:51:35 +0200

    Feature: Added check_if_directory_exists method

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import os
 def create_output_directory(dir_name):
     Path(dir_name).mkdir(parents=True, exist_ok=True)
 
+def check_if_directory_exists(dir_name):
+    return os.path.isdir(dir_name)
 
 def save_output_to_json_file(dir_name, filename, output):
     create_output_directory(dir_name)
@@ -17,8 +19,8 @@ def extract_cat_num_from_filename(filename):
     category_num = filename.split('.')[0]
     return int(category_num)
 
-def read_auctions_from_json():
-    file_list = list(Path('auction_scraper_output').glob("*.json"))
+def read_auctions_from_json(auctions_dir_name):
+    file_list = list(Path(auctions_dir_name).glob("*.json"))
     auctions = {}
     for file in file_list:
         cat_num = extract_cat_num_from_filename(file.name)
